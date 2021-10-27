58f4a947bfb7972a247452854dee3f6e6538b9c7
John Hwang <juhanedang@gmail.com>
2021-10-27 15:09:01 -0400

    setup framework

---
+++ client/src/components/Subcomponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Subcomponent = (props) => (
+  <div>Hello</div>
+)
+
+export default Subcomponent;
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening at localhost:${PORT}!`);
+});
