62dfffad7de7d0f1779e025a17dd4ef8c0399650
Youngkap <kyle.y.hahn@gmail.com>
2021-10-28 10:54:45 -0400

    adding styled-components

--- client/src/components/Overview/OverviewIndex.jsx
+++ client/src/components/Overview/OverviewIndex.jsx
@@ -1,1 +1,29 @@
 // Overview
+
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+import axios from "axios";
+
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productList: null,
+      productSelected: 0
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>{this.state.productList[this.state.productSelected].name}</h1>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<Overview />, document.getElementById('app'));
