27d071c7cd1c68d31d243f9747301119ef0498a4
John Hwang <juhanedang@gmail.com>
2021-10-28 10:53:01 -0400

    import API_KEY into server/index.js and install styled-components

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const config = require('../config.js');
+const API_KEY = config.API_KEY;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,6 +10,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/dist'));
 
 // attach authorization header with API key imported in from config.js file here or in helper js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening at localhost:${PORT}!`);
