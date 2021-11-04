6e62be598edbac6fa41af1a3452fbd0677eb9479
John Hwang <juhanedang@gmail.com>
2021-11-04 17:03:32 -0400

    voting helpful once only functionality implemented

--- client/src/components/R&R/ReviewTile.jsx
+++ client/src/components/R&R/ReviewTile.jsx
@@ -5,46 +5,57 @@ import handler from '../../helpers/reviewhandler.js';
 import dateFormatter from '../../helpers/dateformatter.js';
 
 
+class ReviewTile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checker: false,
+      reported: false
+    }
+  }
 
+  handleClick = (e) => {
+    let { review, updateReviews } = this.props
 
-
-const ReviewTile = ({ review, updateReviews }) => {
-
-
-
-  // stars, filter reviews by rating, show characteristics, adding a question/review (XXXXL)
-  // styled-components
-
-  // 39333 to 40343
-  const handleClick = (e) => {
-    console.log('handleClick target', e.target.getAttribute('name'));
     let action = e.target.getAttribute('name');
-    handler.update(review.review_id, action, (responseData) => {
-      updateReviews();
-    })
+    console.log('action >>', action, 'checker >>', this.state.checker);
+
+    if (action === 'helpful' && this.state.checker === false) {
+
+      this.setState({ checker: true });
+      handler.update(review.review_id, action, (responseData) => {
+        updateReviews();
+      })
+    }
+    if (action === 'report' && this.state.reported === false) {
+      this.setState({ reported: true });
+      handler.update(review.review_id, action, (responseData) => {
+        updateReviews();
+      })
+    }
 
   }
 
-  return (
-    <div>
-      <br/>
-      {review.rating} stars {review.reviewer_name}, {dateFormatter(review.date)}
-      <br />
-      {review.summary}
-      <br />
-      {review.body}
-      <br/>
-      {review.recommend && <div>✅  I recommend this product</div>}
-      <span>Helpful? </span>
-      <span name="helpful" onClick={(e) => handleClick(e)} style={{textDecoration: 'underline'}}>Yes</span>
-      <span> {review.helpfulness} |  </span>
-      <span name="report" onClick={(e) => handleClick(e)} style={{textDecoration: 'underline'}}>Report</span>
-    </div>
+  render() {
+    let { review, updateReviews } = this.props
 
-
-  );
-
+    return (
+      <div>
+        <br/>
+        {review.rating} stars {review.reviewer_name}, {dateFormatter(review.date)}
+        <br />
+        {review.summary}
+        <br />
+        {review.body}
+        <br/>
+        {review.recommend && <div>✅  I recommend this product</div>}
+        <span>Helpful? </span>
+        <span name="helpful" onClick={(e) => this.handleClick(e)} style={{textDecoration: 'underline'}}>Yes</span>
+        <span> {review.helpfulness} |  </span>
+        <span name="report" onClick={(e) => this.handleClick(e)} style={{textDecoration: 'underline'}}>Report</span>
+      </div>
+    )
+  }
 }
-
 
 export default ReviewTile;
