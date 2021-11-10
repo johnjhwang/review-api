e53fa192bc0c6cdee59a8f91979af7f3a24f063e
John Hwang <juhanedang@gmail.com>
2021-11-09 20:40:09 -0500

    form fleshing out

--- client/src/components/R&R/NewReview.jsx
+++ client/src/components/R&R/NewReview.jsx
@@ -27,27 +27,24 @@ class NewReview extends React.Component {
     this.handleInputChange = this.handleInputChange.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
     this.submitReview = this.submitReview.bind(this);
-    this.displayRating = this.displayRating.bind(this);
   }
 
-  displayRating() {
-    let { stars } = this.state;
-    if (stars === 1) {
-
-    }
+  componentDidMount(){
 
   }
+
 
   handleInputChange(e) {
     const name = e.target.getAttribute('name');
-    const value = e.target.getAttribute('value');
-    console.log('e.target >>>>', e.target.getAttribute('name'));
+    const value = e.target.value || e.target.getAttribute('value');
+
+    console.log('e.target >>>>', e.target);
     this.setState({ [name]: value }, () => console.log(`${name} in state is now: `, this.state[name]));
   }
 
 
   submitReview() {
-
+    console.log('submit button clicked, current state >>>>', this.state)
   }
 
   toggleModal() {
@@ -57,10 +54,9 @@ class NewReview extends React.Component {
     })
   }
 
-  // styled-components
   // 39333 to 40343
   render () {
-    let ratingArr = ['Poor', 'Fair', 'Average', 'Good', 'Great']
+    let ratingArr = ['Poor', 'Fair', 'Average', 'Good', 'Great'];
 
     if (this.state.show) {
       return (
@@ -102,22 +98,76 @@ class NewReview extends React.Component {
               </label>
 
               <label>
-                <h4>Number of guests:</h4>
-                <input
-                  name="numberOfGuests"
-                  type="number"
-                  value={this.state.numberOfGuests}
-                  onChange={this.handleInputChange} />
+                <h4>Characteristics</h4>
               </label>
 
+              <label>
+                <h4>Review Summary</h4>
+                <input
+                  style={{width: '50%'}}
+                  type="text"
+                  value={this.state.summary}
+                  name="summary"
+                  onChange={this.handleInputChange}
+                  placeholder="Example: Best purchase ever!"
+                  maxLength="60"
+                  />
+              </label>
+              <label>
+                <h4>Review Body</h4>
+                <textarea
+                style={{width: '95%'}}
+                rows="6"
+                name="body"
+                placeholder="Why did you like the product or not?"
+                value={this.state.body}
+                onChange={this.handleInputChange}/>
+              </label>
+
+              <label>
+                <Button style={{cursor: 'pointer'}} onClick={this.handlePhotos}>Upload Photos</Button>
+              </label>
+              {/* <form method="post" enctype="multipart/form-data" action='/api/csv'>
+              <div>
+                <label for="csv">Upload CSV here:</label>
+                <input type="file" id="csv" name="csv" accept=".csv"/>
+              </div>
+              <div>
+                <button>Submit</button>
+              </div>
+            </form> */}
 
 
-
-
+              <label>
+                <h4>Nickname</h4>
+                <input
+                  style={{width: '50%'}}
+                  type="text"
+                  value={this.state.nickname}
+                  name="nickname"
+                  onChange={this.handleInputChange}
+                  placeholder="Example: jackson11!"
+                  maxLength="60"
+                  />
+                  <br/>{'For privacy reasons, do not use your full name or email address'}
+              </label>
+              <label>
+                <h4>Email</h4>
+                <input
+                  style={{width: '50%'}}
+                  type="text"
+                  value={this.state.email}
+                  name="email"
+                  onChange={this.handleInputChange}
+                  placeholder="Example: jackson11@email.com"
+                  maxLength="60"
+                  />
+                  <br/>{'For authetication reasons, you will not be emailed'}
+              </label>
             </form>
 
             <br />
-            <div style={{display: 'inline'}}><Button onClick={this.toggleModal}>Close</Button>&nbsp; &nbsp; &nbsp;<Button style={{cursor: 'pointer'}} onClick={this.submitReview}>Submit Review</Button></div>
+            <div style={{display: 'inline'}}><Button style={{cursor: 'pointer'}} onClick={this.toggleModal}>Close</Button>&nbsp; &nbsp; &nbsp;<Button style={{cursor: 'pointer'}} onClick={this.submitReview}>Submit Review</Button></div>
           </ModalWrapper>
         </Background>
         );
@@ -151,14 +201,17 @@ const Background = styled.div`
 
 const ModalWrapper = styled.div`
   width: 70%;
-  height: 70%;
+  height: 80%;
   border-radius: 12px;
   background-color: white;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
   display: flex;
   flex-direction: column;
-  padding: 25px;
+  padding: 20px;
+  overflow-y: scroll;
 `;
+
+// max-height: 70vh;
 
 const ExitButton = styled.div`
 display: flex;
