848a2a9a504fb4d4447a0c311dda09093866090f
John Hwang <juhanedang@gmail.com>
2021-12-16 12:13:37 -0500

    docker

--- client/src/components/styles/QA/AddAnswerModal.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const InputContainer = styled.div`
-  padding: 10px;
-  background-color: hsla(205, 37%, 60%, 1);
-`
--- client/src/components/styles/QA/QuestionsListContainer.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const QuestionsListContainer = styled.div`
-  max-height: 80vh;
-  overflow-y: auto;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`
